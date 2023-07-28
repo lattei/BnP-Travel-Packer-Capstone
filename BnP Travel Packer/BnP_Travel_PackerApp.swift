027598a832650f066289ee3092d79bf643944929
Carline <carlinesenat@gmail.com>
2023-07-27 23:25:21 -0700

    Initial Commit

---
+++ BnP Travel Packer/BnP_Travel_PackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BnP_Travel_PackerApp.swift
+//  BnP Travel Packer
+//
+//  Created by Carline Senat on 2023/07/27.
+//
+
+import SwiftUI
+
+@main
+struct BnP_Travel_PackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
